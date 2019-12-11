acf22e77e8a9e4cd89b77a896bb198de13640620
f.telazka@pirxon.com <kWSWOr1YzAJRsj6VFNRv>
2019-12-11 21:15:58 +0100

    Cleanup

--- control.lua
+++ control.lua
@@ -1,13 +1,7 @@
 --control.lua
 
-script.on_event({defines.events.on_tick},
+script.on_event({defines.events.on_click},
 	function (e)
-		if e.tick % 30 == 0 then -- reduce how often this runs
-			for index,player in pairs(game.connected_players) do 
-				if player.character and player.get_inventory(defines.inventory.player_armor).get_item_count("fire-armor") >= 1 then
-					player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"}
-				end
-			end
-		end
+
 	end
 )
